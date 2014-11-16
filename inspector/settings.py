876b0ca052f1d4339c3f462b777b9a78b1d7e139
Samael500 <samael500@gmail.com>
2014-11-16 22:43:43 +0300

    feature: add settings to proj

---
+++ inspector/settings.py
@@ -0,0 +1,27 @@
+import os
+import sys
+from os.path import join
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# add dir to path for use import
+path = join(BASE_DIR, 'inspector')
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+# Twitter authentication information
+# must be redefined in settings local
+TWITTER_OAUTH_INFO = dict(
+    app_key='Consumer Key (API Key)',
+    app_secret='Consumer Secret (API Secret)',
+    oauth_token='Access Token',
+    oauth_token_secret='Access Token Secret',
+)
+
+# is debug
+DEBUG = False
+
+try:
+    from settings_local import *  # noqa
+except ImportError:
+    pass
