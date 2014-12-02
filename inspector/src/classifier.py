685c90aef88d76dd099ca7cd1e7f79f4ed50fc4f
Samael500 <samael500@gmail.com>
2014-12-02 21:01:46 +0300

    feature: add classifier class

--- inspector/src/classifier.py
+++ inspector/src/classifier.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import yaml, os, nltk
+import yaml
+import os
+import nltk
 from nltk.classify.naivebayes import NaiveBayesClassifier
 from inspector.settings import DATA_DIR
 
-'''
+
 class Classifier(object):
 
     """ The classifier class for check are message is positive or negative """
@@ -12,92 +14,51 @@ class Classifier(object):
     filenames = ('positive.ru', 'positive.en', 'negative.ru', 'negative.en')
     filename_suffix = '.yaml'
 
-    def __init__(self, *args, **kwargs):
-        self.load_geocodes()
+    training_tweets = []
+    all_words = []
 
-    def get_file_path(self, filename):
+    def __init__(self, *args, **kwargs):
+        self.load_training_data()
+        # train classifier
+        self.word_features = nltk.FreqDist(self.all_words).keys()
+        training_set = nltk.classify.util.apply_features(self.extract_features, self.training_tweets)
+        self.classifier = NaiveBayesClassifier.train(training_set)
+
+    @property
+    def get_file_path(self):
         """ Return file path to training data files """
-        return os.path.join(DATA_DIR, filename + self.filename_suffix)
+        return ((
+            os.path.join(DATA_DIR, filename + self.filename_suffix),
+            filename.split('.')[0]) for filename in self.filenames)
 
     def load_training_data(self):
         """ Init classifier training """
-        for filename in self.filenames:
+        for filepath, sentiment in self.get_file_path:
             try:
-                with open(self.get_file_path(filename), 'r') as training_file:
-                    training_set = yaml.load(training_file)
+                with open(filepath, 'r') as training_file:
+                    training_tweets = yaml.load(training_file)
+                for tweet in training_tweets:
+                    # to filter short words
+                    words = [word.lower() for word in tweet.split() if len(word) >= 3]
+                    self.training_tweets.append((words, sentiment))
+                    self.all_words.extend(words)
             except IOError:
                 pass
-            self.geocodes = {}
-        return self.geocodes
 
-'''
+    def extract_features(self, document):
+        document_words = set(document)
+        features = dict()
+        for word in self.word_features:
+            features['contains(%s)' % word] = (word in document_words)
+        return features
 
-def get_words_in_tweets(tweets):
-    all_words = []
-    for (words, sentiment) in tweets:
-      all_words.extend(words)
-    return all_words
+    def classify_tweet(self, tweet):
+        return self.classifier.classify(self.extract_features(nltk.word_tokenize(tweet)))
 
-
-def get_word_features(wordlist):
-    wordlist = nltk.FreqDist(wordlist)
-    word_features = wordlist.keys()
-    return word_features
-
-
-def read_tweets(fname, t_type):
-    tweets = []
-    with open(os.path.join(DATA_DIR, fname), 'r') as data:
-        for tweet in yaml.load(data):
-            tweets.append([tweet, t_type])
-    return tweets
-
-
-def extract_features(document):
-    document_words = set(document)
-    features = {}
-    for word in word_features:
-      features['contains(%s)' % word] = (word in document_words)
-    return features
-
-
-def classify_tweet(tweet):
-    return \
-        classifier.classify(extract_features(nltk.word_tokenize(tweet)))
-
-
-# read in postive and negative training tweets
-pos_tweets = read_tweets('positive.ru.yaml', 'positive')
-neg_tweets = read_tweets('negative.ru.yaml', 'negative')
-
-# filter away words that are less than 3 letters to form the training data
-tweets = []
-for (words, sentiment) in pos_tweets + neg_tweets:
-    words_filtered = [e.lower() for e in words.split() if len(e) >= 3]
-    tweets.append((words_filtered, sentiment))
-
-
-# extract the word features out from the training data
-word_features = get_word_features(\
-                    get_words_in_tweets(tweets))
-
-
-# get the training set and train the Naive Bayes Classifier
-training_set = nltk.classify.util.apply_features(extract_features, tweets)
-classifier = NaiveBayesClassifier.train(training_set)
-
-
-# read in the test tweets and check accuracy
-# to add your own test tweets, add them in the respective files
-"""
-test_tweets = read_tweets('happy_test.txt', 'positive')
-test_tweets.extend(read_tweets('sad_test.txt', 'negative'))
-total = accuracy = float(len(test_tweets))
-for tweet in test_tweets:
-    if classify_tweet(tweet[0]) != tweet[1]:
-        print classify_tweet(tweet[0]), tweet[0]
-        accuracy -= 1
-
-print('Total accuracy: %f%% (%d/%d).' % (accuracy / total * 100, accuracy, total))
-
-"""
+    def classify_coord_tweets(self, tweets):
+        for tweet in tweets:
+            sentiment = self.classify_tweet(tweet[0])
+            tweet.append(sentiment)
+            print tweet
+        print tweets
+        return tweets
